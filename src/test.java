c48d43fa0c9819b6fbcd2da7388a9d520130e54f
Nifilia <Anderokore@gmail.com>
2021-01-11 15:49:47 +0200

    Second Commit

---
+++ src/test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main (String[] args){
+        int[] vastuvoetud = {2803, 2626, 2359, 1927, 2236, 2281, 2394, 2484, 2468};
+        Scanner sisend = new Scanner(System.in);
+        System.out.println("Sisesta aasta: ");
+        int aasta = sisend.nextInt();
+        int massiiviIndeks = aasta - 2011;
+        System.out.println();
+    }
+}
