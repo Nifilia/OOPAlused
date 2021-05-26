0b4a501911cb75a64293811eb315f5c85e513846
Nifilia <Anderokore@gmail.com>
2021-05-26 08:23:51 +0300

    Massivid - Ül 5.4c - tahvli juurde

--- src/Test.java
+++ src/Test.java
@@ -1,16 +1,39 @@
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Test {
-    static int mahlapakkidearv(double ounteKogus) {
-        int pakkidArv = (int)Math.round(ounteKogus * 0.4 / 3);
-        return pakkidArv;
-    }
-    public static void main(String[] args) {
-        Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisesta õunte kogus kilogrammides: ");
+    public static void main(String[] args){
+        ArrayList<String> opilased = new ArrayList<>();
+        File fail = new File("C:\\Users\\Andero\\IdeaProjects\\oop_alused\\OOPAlused\\src\\nimekiri.txt");
+        Scanner sisendFailist = null;
+        try {
+            sisendFailist = new Scanner(fail);
+        } catch (Exception e){
+            System.out.println("Faili pole - " + e.getMessage());
+        }
 
-        double ountekogus = sisend.nextDouble();
+        while (sisendFailist.hasNextLine()){
+            String rida = sisendFailist.nextLine();
+            opilased.add(rida);
+        }
+        sisendFailist.close();
 
-        System.out.println(mahlapakkidearv(ountekogus));
+        LocalDateTime tananeKuupaev = LocalDateTime.now();
+        System.out.println("Tänane kuupäev ilma vormistuseta on: " + tananeKuupaev);
+        DateTimeFormatter kuupaevaVormindus = DateTimeFormatter.ofPattern("dd/MM/yyy HH:MM:ss");
+        String tananeKuupaevVormistatud = tananeKuupaev.format(kuupaevaVormindus);
+        System.out.println("tänane vormistatud kuupäev on: " + tananeKuupaevVormistatud);
+
+        int indeks = Integer.parseInt(tananeKuupaevVormistatud);
+        System.out.println(opilased.get(indeks - 1));
+        /*
+        for (int i = 0; i < opilased.size(); i++){
+            System.out.println(i + " " + opilased.get(i));
+        }
+
+         */
     }
 }
