9402168980416d10f37afb9e43507879886f1423
Nifilia <Anderokore@gmail.com>
2021-05-26 03:27:00 +0300

    Ül 3.3 - variant 1

--- src/Test.java
+++ src/Test.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
         Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisesta, mitu korda tuleb äratada: ");
+        System.out.println("Täringute arv: ");
 
-        int mituKorda = sisend.nextInt();
-        int kord = 1;
-
-        while (kord <= mituKorda) {
-            System.out.println("Tõuse ja sära!");
-            kord++;
+        int taringuteArv = sisend.nextInt();
+        int taring = (int)Math.round(Math.random() * 6);
+        System.out.println(taring);
+        while(taring != taringuteArv){
+            taring = (int)Math.round(Math.random() * 6);
+            System.out.println(taring);
         }
     }
 }
